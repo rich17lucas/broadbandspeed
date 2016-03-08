77d8968e7b8b4ec07ba2546ce55635371f90e8b1
rich17lucas <git@caymancomputers.co.uk>
2016-03-08 15:28:36 +0000

    Introduced the use of functions to make the main method easier to read

--- CheckBroadbandSpeed.py
+++ CheckBroadbandSpeed.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+import subprocess
+import re
+import datetime
+import csv
+from ConfigParser import SafeConfigParser
+import os
+import sys
+
 """
 @Name:      CheckBroadbandSpeed.py
 @Author:    Richard Lucas
@@ -14,46 +22,20 @@
             
             Also the script will introduce error handling, and using functions.
 """
-import subprocess
-import re
-import datetime
-import csv
-from ConfigParser import SafeConfigParser
-import os
-import sys
-
-try:
-    parser = SafeConfigParser()
-    parser.read(os.path.join(os.path.dirname(__file__),"config.ini"))
-except:
-    print("Error: Could not read file 'config.ini'. Please check it exists in the same directory as CheckBroad")
-    sys.exit(1)
-    
+#------------------------------------------------------------------------------
 ## Global variables
-# Directory where this script is located
-BROADBAND_SPEED_HOME = parser.get('location','broadband_speed_home')
-# The path and name of the data file.
-DATAFILE_HOME =  parser.get('location','datafile')
-# The location of the speedtest package that was installed via pip
-SPEEDTEST_HOME = parser.get('location','speedtest')
-# Set test=True for testing; Test=False for real measurements in config.ini
-TEST=parser.getboolean('mode','test')
+#------------------------------------------------------------------------------
 
-# Start time
-#NOW = datetime.datetime.now().strftime("%Y-%b-%d %H:%M")
-NOWDATE = datetime.datetime.now().strftime("%Y-%b-%d")
-NOWTIME = datetime.datetime.now().strftime("%H:%M")
 
-# Test the TEST condition
-if( TEST == False):
-    """If TEST is false, then run the speedtest-cli module to
-        obtain actual numbers for the ISP """
-    result = str(subprocess.check_output(SPEEDTEST_HOME))
-else:
-    """If TEST is true, then use this canned data"""
-    result = """Retrieving speedtest.net configuration...
+#------------------------------------------------------------------------------
+
+class CannedData(object):
+    '''This class stores some test data for testing changes to the code
+    without the need to peform a real test'''
+    
+    cannedData = """Retrieving speedtest.net configuration...
 Retrieving speedtest.net server list...
-Testing from PlusNet Technologies Ltd (212.159.103.14)...
+Testing from PlusNet Technologies Ltd (67.198.33.99)...
 Selecting best server based on latency...
 Hosted by Fasthosts Internet (Gloucester) [29.59 km]: 19.812 ms
 Testing download speed........................................
@@ -61,29 +43,75 @@ Download: 31.44 Mbit/s
 Testing upload speed..................................................
 Upload: 6.00 Mbit/s"""
 
+    @classmethod
+    def getCannedData(cls):
+        return cls.cannedData
 
+#------------------------------------------------------------------------------
+def getSpeedtestResult():
+    '''
+    Returns the result of the speedtest.
+    If the test mode is enabled in the config.ini (test=True)then it will return
+    the canned data
+    '''
+    if( TEST == False):
+        """If TEST is false, then run the speedtest-cli module to
+            obtain actual numbers for the ISP """
+        return str(subprocess.check_output(SPEEDTEST_HOME))
+    else:
+        """If TEST is true, then use this canned data"""
+        return CannedData.getCannedData()
+
+#------------------------------------------------------------------------------
+
+def getMatches(result, rxPattern, rxGroups):
+    '''
+    result is the speedtest result string
+    rxPattern is the regular expression to use
+    rxGroups is the List of match groups to return
+    
+    example:
+    ISP, IPADDR = getMatches(speedTestResult, "^Testing from (.*) \((.*)\)", (1,2))
+    '''
+    pattern = re.compile(rxPattern, re.MULTILINE)
+    matches = pattern.search(result)
+    return matches.groups(rxGroups)
+
+#===============================================================================
+# Main
+try:
+    parser = SafeConfigParser()
+    parser.read(os.path.join(os.path.dirname(__file__),"config.ini"))
+except:
+    print("Error: Could not read file 'config.ini'. Please check it exists in the same directory as CheckBroad")
+    sys.exit(1)
+
+# Directory where this script is located - configured in config.ini
+BROADBAND_SPEED_HOME = parser.get('location','broadband_speed_home')
+# The path and name of the data file - configured in config.ini
+DATAFILE_HOME =  parser.get('location','datafile')
+# The location of the speedtest package that was installed via pip  - configured in config.ini
+SPEEDTEST_HOME = parser.get('location','speedtest')
+# Set test=True for testing; Test=False for real measurements in config.ini
+TEST=parser.getboolean('mode','test')
+# Start time
+NOWDATE = datetime.datetime.now().strftime("%Y-%b-%d")
+NOWTIME = datetime.datetime.now().strftime("%H:%M")    
+
+"""Run the Speedtest"""
+speedTestResult = getSpeedtestResult()
+    
 """Get ISP and IP Address"""
-pattern = re.compile("^Testing from (.*) \((.*)\)", re.MULTILINE)
-matches = pattern.search(result)
-ISP = matches.group(1)
-IPADDR = matches.group(2)
+ISP, IPADDR = getMatches(speedTestResult, "^Testing from (.*) \((.*)\)", (1,2))
 
 """Get Host, distance and Ping"""
-pattern = re.compile("^Hosted by (.*).\[(\d*.\d*).*\]: (\d*.\d*)", re.MULTILINE)
-matches = pattern.search(result)
-HOST = matches.group(1)
-DISTANCE = matches.group(2)
-PING = matches.group(3)
+HOST, DISTANCE, PING = getMatches(speedTestResult, "^Hosted by (.*).\[(\d*.\d*).*\]: (\d*.\d*)", (1,2,3))
 
 """Get Download speed"""
-pattern = re.compile("^Download:\s+(\d+.\d+)", re.MULTILINE)
-matches = pattern.search(result)
-DOWNLOAD = matches.group(1)
+DOWNLOAD = getMatches(speedTestResult, "^Download:\s+(\d+.\d+)", (1))
 
 """Get Upload speed"""
-pattern = re.compile("^Upload: (\d+.\d+)", re.MULTILINE)
-matches = pattern.search(result)
-UPLOAD = matches.group(1)
+UPLOAD = getMatches(speedTestResult, "^Upload: (\d+.\d+)", (1))
 
 if (TEST == True):
     """Print the results to STDOUT """
