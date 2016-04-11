f9661ec557aec57b960625ac6940e2f023780247
rich17lucas <richard.lucas@caymancomputers.co.uk>
2016-04-11 11:31:49 +0100

    Testing out assertions-  more work needs doing with the download and upload variables with turn out to be 'tuples'. Need to find out why the regex getMatches returns a tuple for single value and how to handle it.

---
+++ config.ini.nix
@@ -0,0 +1,9 @@
+[location]
+;The path and name of the data file.
+datafile=/media/rnslucas/e_drive/work/broadbandspeed/data/data.csv
+;The location of the speedtest package that was installed sudo apt-get install speedtest-cli
+speedtest=/usr/bin/speedtest-cli
+    
+[mode]
+# Set test=True for testing; Test=False for real measurements
+test=True
